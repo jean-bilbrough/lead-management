83c832199e0af795de6c5426d45cab39897d27f6
Jean <jeanbilbrough@hotmail.com>
2020-10-27 12:27:35 +1100

    remove unused from App

--- lead-management-ui/src/App.tsx
+++ lead-management-ui/src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { ThemeProvider  } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
 import LeadManagementRoot from './LeadManagementRoot';
-import theme from './styles/leadManagementTheme';
 
 function App() {  
-  return(<ThemeProvider theme={theme}><Grid container><LeadManagementRoot /></Grid></ThemeProvider>);
+  return(<LeadManagementRoot />);
 }
 
 export default App;
